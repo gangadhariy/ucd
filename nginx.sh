f01c81bdd06a11f88f02bcc73081cfae97b19a7b
gangadhariy <121916303+gangadhariy@users.noreply.github.com>
2023-07-25 16:45:39 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install nginx -y
+sudo yum install nginx  httpd -y
+echo "hello world" >> /var/www/html/index.html
+sudo systemctl start httpd
 sudo systemctl start nginx
